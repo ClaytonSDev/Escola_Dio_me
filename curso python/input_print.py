47bf52d5fb4d4c5df42dc767a5568eaec4b56955
Clayton Sales <claytonsalesdev@gmail.com>
2024-04-09 19:40:34 -0300

    .
    .

--- curso python/input_print.py
+++ curso python/input_print.py
@@ -1,8 +1,12 @@
 nome = input('Informe o seu nome: ')
 idade = input('Infomre a sua idade: ')
+sobrenome = input('Informe seu sobrenome:')
 
 print(nome, idade)
 print(nome, idade, end="...\n")
 print(nome, idade, sep="#", end="...\n")
 print(nome, idade, sep="#")
+print(nome, sobrenome, idade)
+
+
 
