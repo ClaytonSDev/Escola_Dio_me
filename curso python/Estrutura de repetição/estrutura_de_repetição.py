5f7ee10e532041e8085fa08635aea633296b1b59
Clayton Sales <claytonsalesdev@gmail.com>
2024-04-11 22:03:02 -0300

    .
    .

---
+++ curso python/Estrutura de repetição/estrutura_de_repetição.py
@@ -0,0 +1,14 @@
+texto = input("informe um texto: ")
+VOGAIS = "AEIOU"
+
+for letra in texto:
+    if letra.upper() in VOGAIS:
+        print(letra, end="")
+else:
+    print()
+    print("Executa no final do laço")
+
+
+# Exemplo utilizando a função built-in range
+for numero in range(0, 51, 5):
+    print(numero, end=" ")
